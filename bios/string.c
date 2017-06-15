6652a875d791c99c2d041d141ad7229fb580f8a4
Ahmad Fatoum <ahmad@a3f.at>
2017-06-15 11:19:02 +0200

    Add basic memory manipulation functions
    mostly, so I can call them in gdb

--- bios/string.c
+++ bios/string.c
@@ -1,4 +1,5 @@
 #include "string.h"
+#include <stddef.h>
 char* itopow2(uint32_t num, int pow2, char *s, size_t size)
 {
 	long len = (long) size - 1;
@@ -38,5 +39,81 @@ char* itodec(uint32_t num, char *s, size_t size)
 	}
 
 	return &s[i+1];
+}
+
+void *memchr(const void *_ptr, int ch, size_t len)
+{
+	const unsigned char *ptr = _ptr;
+    for (size_t i = 0; i < len; i++)
+        if(ptr[i] == ch)
+            return (void*)&ptr[i];
+
+    return NULL;
+}
+
+void *memset(void *_dst, int ch, size_t len)
+{
+    unsigned char *dst = _dst;
+
+    while (len--)
+        *dst++ = (unsigned char)ch;
+
+    return _dst;
+}
+
+int memcmp(const void *_s1, const void *_s2, size_t len)
+{
+	const unsigned char *s1 = _s1, *s2 = _s2;
+	
+    while (len--) {
+        unsigned char ch1 = *s1++,
+                      ch2 = *s2++;
+        if (ch1 != ch2)
+            return ch1 - ch2;
+    }
+
+    return 0;
+}
+
+void *memcpy(void * restrict _dst, const void * restrict _src, size_t len)
+{
+	      unsigned char *dst = _dst;
+	const unsigned char *src = _src;
+
+	if (!len) return _dst;
+
+    size_t n = (len + 7) >> 3;
+    switch (len & 7) {
+    case 0: do { *dst++ = *src++;
+    case 7:      *dst++ = *src++;
+    case 6:      *dst++ = *src++;
+    case 5:      *dst++ = *src++;
+    case 4:      *dst++ = *src++;
+    case 3:      *dst++ = *src++;
+    case 2:      *dst++ = *src++;
+    case 1:      *dst++ = *src++;
+            } while (--n);
+    }
+
+	return _dst;
+}
+
+void *memmove(void *_dst, const void *_src, size_t len)
+{
+	      unsigned char *dst = _dst;
+	const unsigned char *src = _src;
+
+	if (!len) return _dst;
+
+	if (dst <= src)
+		return memcpy(dst, src, len);
+
+	src += len;
+	dst += len;
+
+	while (len--)
+		*--dst = *--src;
+
+	return _dst;
 }
 
--- bios/string.h
+++ bios/string.h
@@ -14,4 +14,21 @@ static inline size_t strlen(const char *s)
     return n;
 }
 
+void *memchr(const void *ptr, int ch, size_t len);
+void *memset(void *_dst, int ch, size_t len);
+void *memchr(const void *ptr, int ch, size_t len);
+int memcmp(const void *s1, const void *s2, size_t len);
+void *memcpy(void *restrict dst, const void *restrict src, size_t len);
+static inline void *mempcpy(void * restrict dst, const void * restrict src, size_t len)
+{
+    return (char*)memcpy(dst, src, len) + len;
+}
+void *memmove(void *dst, const void * src, size_t len);
+static inline void *mempmove(void *dst, const void *src, size_t len)
+{
+    return (char*)memmove(dst, src, len) + len;
+}
+
+
 #endif
+
