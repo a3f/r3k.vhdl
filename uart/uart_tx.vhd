74ac0350b97920ad2702cbfce3c563f9f4a9bb0c
ucedp <ucedp@student.kit.edu>
2017-06-22 13:10:50 +0200

    ADD: uart_tx

--- uart/uart_tx.vhd
+++ uart/uart_tx.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_misc.all;
 use ieee.numeric_std.all;
 
-entity uart_rx is 
+entity uart_tx is 
 generic (
         idle : INTEGER( 1  downto 0  ) := 0 ;
         start : INTEGER( 1  downto 0  ) := 1 ;
@@ -14,23 +14,26 @@ generic (
      port (
         clk :  in std_logic;
         reset :  in std_logic;
-        rx :  in std_logic;
+        tx_start :  in std_logic;
         baud_tick :  in std_logic;
-        rx_done_tick :  out std_logic;
-        rx_data :  out std_logic_vector( 7  downto 0  )
+        tx_data :  in std_logic_vector( 7  downto 0  );
+        tx_done_tick :  out std_logic;
+        tx :  out std_logic
     );
 end entity; 
 
 
-architecture rtl of uart_rx is 
+architecture rtl of uart_tx is 
     signal state_reg : std_logic_vector( 1  downto 0  );
     signal state_next : std_logic_vector( 1  downto 0  );
     signal baud_reg : std_logic_vector( 4  downto 0  );
     signal baud_next : std_logic_vector( 4  downto 0  );
-    signal n_reg : std_logic_vector( 3  downto 0  );
-    signal n_next : std_logic_vector( 3  downto 0  );
+    signal n_reg : std_logic_vector( 4  downto 0  );
+    signal n_next : std_logic_vector( 4  downto 0  );
     signal d_reg : std_logic_vector( 7  downto 0  );
     signal d_next : std_logic_vector( 7  downto 0  );
+    signal tx_reg : std_logic;
+    signal tx_next : std_logic;
     begin 
         process 
         begin
@@ -40,34 +43,40 @@ architecture rtl of uart_rx is
                 baud_reg <= 0 ;
                 n_reg <= 0 ;
                 d_reg <= 0 ;
+                tx_reg <= '1';
             else 
                 state_reg <= state_next;
                 baud_reg <= baud_next;
                 n_reg <= n_next;
                 d_reg <= d_next;
+                tx_reg <= tx_next;
             end if;
         end process;
         process 
         begin
             wait ;
             state_next <= state_reg;
-            rx_done_tick <= '0';
+            tx_done_tick <= '0';
             baud_next <= baud_reg;
             n_next <= n_reg;
             d_next <= d_reg;
+            tx_next <= tx_reg;
             case  ( state_reg ) is 
                 when 
                     idle => 
-                    if ( (  not rx )  ) then 
+                    tx_next <= '1';
+                    if ( tx_start ) then 
                         state_next <= start;
                         baud_next <= 0 ;
+                        d_next <= tx_data;
                     end if;
                 when 
                     start => 
+                    tx_next <= '0';
                     if ( baud_tick ) then 
                         baud_next <= ( baud_reg + 1  ) ;
                     else 
-                        if ( ( baud_reg = 8  )  ) then 
+                        if ( ( baud_reg = 16  )  ) then 
                             state_next <= data;
                             baud_next <= 0 ;
                             n_next <= 0 ;
@@ -75,13 +84,14 @@ architecture rtl of uart_rx is
                     end if;
                 when 
                     data => 
+                    tx_next <= d_reg(0 );
                     if ( baud_tick ) then 
                         baud_next <= ( baud_reg + 1  ) ;
                     else 
                         if ( ( baud_reg = 16  )  ) then 
-                            d_next <= ( rx & d_reg(7  downto 1 ) );
-                            n_next <= ( n_reg + 1  ) ;
+                            d_next <= ( d_reg srl 1  ) ;
                             baud_next <= 0 ;
+                            n_next <= ( n_reg + 1  ) ;
                         else 
                             if ( ( n_reg = 8  )  ) then 
                                 state_next <= stop;
@@ -90,17 +100,18 @@ architecture rtl of uart_rx is
                     end if;
                 when 
                     stop => 
+                    tx_next <= '1';
                     if ( baud_tick ) then 
                         baud_next <= ( baud_reg + 1  ) ;
                     else 
                         if ( ( baud_reg = 16  )  ) then 
                             state_next <= idle;
-                            rx_done_tick <= '1';
+                            tx_done_tick <= '1';
                         end if;
                     end if;
             end case;
         end process;
-        rx_data <= d_reg;
+        tx <= tx_reg;
     end; 
 
 
