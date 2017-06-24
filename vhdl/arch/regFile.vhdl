e91e4c1b870082ba17c0ab7f2eff26517e9043ff
Ahmad Fatoum <ahmad@a3f.at>
2017-06-24 23:17:56 +0200

    Implement register file

--- vhdl/arch/regFile.vhdl
+++ vhdl/arch/regFile.vhdl
@@ -1,24 +1,41 @@
-brary ieee;
+library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.arch_defs.all;
 
 entity regFile is
 		port (
 		-- static
-		readreg1 : in  std_logic_vector(4 downto 0);		
-		readreg2 : in  std_logic_vector(4 downto 0);
-		writereg: in std_logic_vector (4 downto 0);
-		writedata: in std_logic_vector (31 downto 0);
-		readData1 : out std_logic_vector (31 downto 0);
-		readData2 : out std_logic_vector (31 downto 0);
+		readreg1, readreg2 : in reg_t;
+		writereg: in reg_t;
+		writedata: in word_t;
+		readData1, readData2 : out word_t;
 		clk : in std_logic;
-		regWrite : in std_logic;
+		regWrite : in std_logic
 		);
-	end regFile ;
+	end regFile;
 		
-architecture leds of leds is
+architecture behav of regFile is
+    type regfile_t is array (31 downto 0) of word_t;
+    signal reg : regfile_t := (R0 => ZERO);
 
 
-begin process(clk) begin
+begin process(readreg1, readreg2, writereg, clk)
+        variable readreg1_int, readreg2_int, writereg_int : integer;
+    begin
+    -- Do these need to be inside a process? I think not.
+    if readreg1'event then
+        readdata1 <= reg(readreg1_int);
+    end if;
+    if readreg1'event then
+        readdata2 <= reg(readreg2_int);
+    end if;
+    if rising_edge(clk) then
+        if regWrite = '1' then
+                regs(writereg_int) <= writedata;
+        end if;
+    end if;
 
-end regFile;
+end process;
+
+end behav;
