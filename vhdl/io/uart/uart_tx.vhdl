4f61ad67c9103ff9926c7b66f6544549009c4185
Ahmad Fatoum <ahmad@a3f.at>
2017-07-07 18:17:50 +0200

    Build io/ first when running tests
    This fixes that cryptic
    error: entity "uart_tx" is obsoleted by package "arch_defs"

--- vhdl/io/uart/uart_tx.vhdl
+++ vhdl/io/uart/uart_tx.vhdl
@@ -2,7 +2,6 @@ library ieee;
 library work;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.std_logic_misc.all;
 use work.arch_defs.all;
 use work.utils.all;
 
--- vhdl/tb/uart_tb.vhdl
+++ vhdl/tb/uart_tb.vhdl
@@ -2,54 +2,69 @@ library ieee;
 library work;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.std_logic_misc.all;
 use work.arch_defs.all;
 use work.utils.all;
 
 entity uart_tb is
-port(
-	baud_tick: in std_logic; --19200
-	clk: in std_logic --system clock
-);
-end uart_tb;
+    end uart_tb;
 
 architecture behav of uart_tb is
 
-component uart_rx is
-port (
-        clk :  in std_logic;
-        reset :  in std_logic;
-        rx :  in std_logic;
-        baud_tick :  in std_logic;
-        rx_done_tick :  out std_logic;
-        rx_data :  out std_logic_vector( 7  downto 0  )
-);
-end component;
-component uart_tx is
-port(
-        clk :  in std_logic;
-        reset :  in std_logic;
-        tx_start :  in std_logic;
-        baud_tick :  in std_logic;
-        tx_data :  in std_logic_vector( 7  downto 0  );
-        tx_done_tick :  out std_logic;
-        tx :  out std_logic
-);
-end component;
-type state_t is (idle, received, transmit);
-signal rx_data: std_logic_vector(7 downto 0);
-signal rx_done_tick, tx_done_tick: std_logic;
-signal tx_data_next, tx_data: std_logic_vector(7 downto 0);
-signal tx_start: std_logic;
-signal reset, rx, tx: std_logic;
-signal state_next, state: state_t;
+    component uart_tx
+    port(
+            clk :  in std_logic;
+            reset :  in std_logic;
+            tx_start :  in std_logic;
+            baud_tick :  in std_logic;
+            tx_data :  in std_logic_vector( 7  downto 0  );
+            tx_done_tick :  out std_logic;
+            tx :  out std_logic
+    );
+    end component;
+    component uart_rx
+    port (
+            clk :  in std_logic;
+            reset :  in std_logic;
+            rx :  in std_logic;
+            baud_tick :  in std_logic;
+            rx_done_tick :  out std_logic;
+            rx_data :  out std_logic_vector( 7  downto 0  )
+    );
+    end component;
+
+    type state_t is (idle, received, transmit);
+    signal rx_data: std_logic_vector(7 downto 0);
+    signal rx_done_tick, tx_done_tick: std_logic;
+    signal tx_data_next, tx_data: std_logic_vector(7 downto 0);
+    signal tx_start: std_logic;
+    signal reset, rx, tx: std_logic;
+    signal state_next, state: state_t;
+
+    signal clk : std_logic; -- system clock
+    signal baud_tick : std_logic; -- 19200
 
 begin
 
-tx_instance: uart_tx
-port map (clk, reset, tx_start, baud_tick, tx_data, tx_done_tick, tx);
-rx_instance: uart_rx
-port map (clk, reset, rx, baud_tick, rx_done_tick, rx_data);
+tx_instance: uart_tx port map (clk, reset, tx_start, baud_tick, tx_data, tx_done_tick, tx);
+
+rx_instance: uart_rx port map (clk, reset, rx, baud_tick, rx_done_tick, rx_data);
+
+clk_process :process
+begin
+    clk <= '0';
+    wait for 1 ns;  --for 0.5 ns signal is '0'.
+    clk <= '1';
+    wait for 1 ns;  --for next 0.5 ns signal is '1'.
+end process;
+
+baud_tick_process :process
+begin
+    baud_tick <= '0';
+    wait for 1 ns;  --for 0.5 ns signal is '0'.
+    baud_tick <= '1';
+    wait for 1 ns;  --for next 0.5 ns signal is '1'.
+end process;
+
 
 reset_ctrl: process (clk, reset) is
 begin
