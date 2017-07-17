98ab2253da621f219abdc996bf1aa2b4d252cc33
Ahmad Fatoum <ahmad@a3f.at>
2017-07-17 23:25:29 +0200

    [Travis CI] Install GHDL 3.3 dependencies

--- install-deps.sh
+++ install-deps.sh
@@ -3,8 +3,8 @@ which dpkg 2>&1 >/dev/null
 
 if [ $? -eq 0 ]; then
     set -e
-    sudo apt-get install libgnat-4.6 || 
-         ( wget http://ftp.de.debian.org/debian/pool/main/g/gnat-4.6/libgnat-4.6_4.6.3-8_amd64.deb -O/tmp/libgnat.deb && sudo dpkg -i /tmp/libgnat.deb)
+    sudo apt-get install libgnat-4.9 || 
+         ( wget http://ftp.de.debian.org/debian/pool/main/g/gnat-4.9/libgnat-4.9_4.9.2-1_amd64.deb -O/tmp/libgnat.deb && sudo dpkg -i /tmp/libgnat.deb)
     wget http://ftp.de.debian.org/debian/pool/main/m/mpclib/libmpc2_0.9-4_amd64.deb -O/tmp/libmpc2.deb
     sudo dpkg -i /tmp/libmpc2.deb
     wget https://sourceforge.net/projects/ghdl-updates/files/Builds/ghdl-0.33/debian/ghdl_0.33-1jessie1_amd64.deb/download -O/tmp/ghdl.deb
