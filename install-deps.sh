f4fe5d54885e0981d40ac2164e7539e5e22d58ee
Ahmad Fatoum <ahmad@a3f.at>
2017-07-17 22:49:55 +0200

    [FIXED] Upgrade GHDL in install-deps.sh to fix Travis CI
    Wrong URL in previous commit

--- install-deps.sh
+++ install-deps.sh
@@ -7,7 +7,7 @@ if [ $? -eq 0 ]; then
          ( wget http://ftp.de.debian.org/debian/pool/main/g/gnat-4.6/libgnat-4.6_4.6.3-8_amd64.deb -O/tmp/libgnat.deb && sudo dpkg -i /tmp/libgnat.deb)
     wget http://ftp.de.debian.org/debian/pool/main/m/mpclib/libmpc2_0.9-4_amd64.deb -O/tmp/libmpc2.deb
     sudo dpkg -i /tmp/libmpc2.deb
-    wget https://sourceforge.net/projects/ghdl-updates/files/Builds/ghdl-0.31/Debian/ghdl_0.33-1jessie1_amd64.deb/download -O/tmp/ghdl.deb
+    wget https://sourceforge.net/projects/ghdl-updates/files/Builds/ghdl-0.33/debian/ghdl_0.33-1jessie1_amd64.deb/download -O/tmp/ghdl.deb
     sudo dpkg -i /tmp/ghdl.deb
     sudo ldconfig
     ghdl --version
