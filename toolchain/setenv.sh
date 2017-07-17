604684add3b589c3cf789fa51271fe635318e223
Ahmad Fatoum <ahmad@a3f.at>
2017-07-17 23:08:05 +0200

    chmod +x executable files

--- toolchain/setenv.sh
+++ toolchain/setenv.sh
@@ -4,8 +4,6 @@
 #    . ./setenv.sh
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/cross/$(uname -m)/bin
-# I know, I know...
-[[ -z "${CI}" ]] && true || chmod +x $DIR/* $DIR/../libexec/gcc/mips/6.2.0/*
 
 echo Adding $DIR to path
 
