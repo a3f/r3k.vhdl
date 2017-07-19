26e551f3d1c93d4eff0f85ff478e24368cce4759
Ahmad Fatoum <ahmad@a3f.at>
2017-07-19 23:16:51 +0200

    Regression fix: assertion error now fails test

--- vhdl/tb/test.pl
+++ vhdl/tb/test.pl
@@ -3,50 +3,56 @@
 use strict;
 no strict 'vars';
 use warnings;
+no warnings 'uninitialized';
 
 @only = split ' ', $ENV{TEST} if defined $ENV{TEST};
 @skip = split ' ', $ENV{SKIP} if defined $ENV{SKIP};
 
 for (@ARGV) {
-    /^-/ and push @opts, $_ or push @tests, $_
+    /^-/ and push @opts, $_ or $tests{$_} = 0;
 }
 
-for (@tests) {
-    open $file, '<', "$_.vhdl"; $line = <$file>; close $file;
-    push @skip, $_ if $line =~ /^\s*--\s*SKIP/i;
+if (@only) {
+    undef %tests;
+    $tests{$_} = 0 for @only; # hash slice?
+} else {
+    for (keys %tests) {
+        open $file, '<', "$_.vhdl"; $line = <$file>; close $file;
+        undef $tests{$_} if $line =~ /^\s*--\s*SKIP/i;
+    }
+    undef $tests{$_} for @skip;
 }
-
-$total = @tests;
-
-%disabled = map {($_, 1)} @skip;
-@tests = grep {!$disabled{$_}} @tests;
-
-@tests = @only if @only;
 
 @aopts = @eopts = @ropts = @opts;
 # push @ropts, '--assert-level=note' if $ENV{FATAL};
 
 sub run { print "@_ \n"; 0 == system @_ }
 
-for (@tests) {
-    $errors++; # Pessimistic by default
+for (keys %tests) {
+    do { $skipped++; next } unless defined $tests{$_};
     run 'ghdl', '-a', @aopts, "$_.vhdl" or next;
     run 'ghdl', '-e', '-g', @eopts, $_, or next;
     @out = qx(ghdl -r @ropts $_ --vcd=$_.vcd 3>&1 1>&2 2>&3 3>&-);
-    @out = grep {
-        $failed++ if /:\(assertion error\):/;
-        !/:\(assertion warning\): NUMERIC_STD/ 
-    } @out if $ENV{NO_WARN_NUMERIC_STD};
+    $? == 0 or next;
+    for $line (@out) {
+        $tests{$_} = -1 if $line =~ /:\(assertion error\):/;
+        print $line if !$ENV{NO_WARN_NUMERIC_STD} || $line !~ /:\(assertion warning\): NUMERIC_STD/ 
+    }
 
-    print @out;
-    $errors-- unless $? != 0 || $failed;
+    $ok++ if ++$tests{$_};
 }
+
+$tests = keys(%tests) - $skipped;
+$errors = $tests - $ok;
+#print "tests=$tests ok=$ok skipped=$skipped errors=$errors\n";
 
 if ($errors) {
-    print "\n \33[31m=> $errors/${\scalar @tests} test(s) failed.\33[m";
+    print "\n \33[31m=> $errors/$tests test(s) failed:\n";
+    print "\t$_\n" for grep { defined $tests{$_} && $tests{$_} == 0 } keys %tests;
+    print" \33[m";
 } else {
-    print "\n \33[32m=> All ${\scalar @tests} tests were successful\33[m";
+    print "\n \33[32m=> All $tests tests were successful\33[m";
 }
-print " => ". ($total - @tests) . " skipped.\n";
+print " => $skipped skipped.\n";
 exit $errors;
 
